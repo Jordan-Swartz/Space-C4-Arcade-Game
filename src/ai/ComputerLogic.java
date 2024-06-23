ef275afa24c3e21d6e8e215fbf6c3a1644969935
Jordan-Swartz <JDSwartz@proton.me>
2024-06-22 23:30:58 -0700

    Computer AI Update

--- src/ai/ComputerLogic.java
+++ src/ai/ComputerLogic.java
@@ -18,5 +18,8 @@ public class ComputerLogic {
         return false;
     }
 
-
+    //start with BFS to do a quick search for winning or blocking oppponent win
+    //switch to DFS if not found to look for best possible move
+    //use token matrices to evaluate potential moves considering long term strat
+    //possible heuristic evaluation to score board state 9 sequences, threats, etc)
 }
---
+++ src/ai/Graph.java
@@ -0,0 +1,15 @@
+package ai;
+
+import java.util.*;
+
+public class Graph {
+    //private LinkedList<Integer> adj[];
+    private final int V;
+    private int E;
+    
+    public Graph(int V) {
+        //adj = new LinkedList<Integer>
+        this.V = V;
+        this.E = 0;
+    }
+}
